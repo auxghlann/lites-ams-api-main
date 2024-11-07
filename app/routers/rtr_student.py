a21f3eeb4235648a17f41b64466bbc0c4f415d28
auxghlann <orenoytemail@gmail.com>
2024-11-07 21:28:20 +0800

    feat & refactor: Add a new router to update student and Refactor student router to handle another layer of error

--- app/routers/rtr_student.py
+++ app/routers/rtr_student.py
@@ -7,7 +7,6 @@ student_router = APIRouter(prefix="/student")
 
 class StudentRecord(BaseModel):
     student_id: int
-    new_student_id: int | None = None
     fName: str
     lName: str
     program: str
@@ -24,18 +23,19 @@ def add_student(student_record: StudentRecord) -> dict:
         program: str = student_record.program
         year: int = student_record.year
 
-        record = MonitorStudent.add_student(stud_id=student_id, fName=fName, lName=lName, program=program, year=year)
+        response = MonitorStudent.add_student(stud_id=student_id, fName=fName, lName=lName, program=program, year=year)
 
-        if record.data:
+        if response.data:
             return {
                 "status_code": 200,
-                "message": "Student Added Successfully"
+                "message": f"Student {student_id} Added Successfully"
             }
         
     except APIError as e:
         if e.code == '23505':
             raise HTTPException(status_code=409, detail=str(e.details))
-
+        else:
+            raise HTTPException(status_code=400, detail=str(e.details))
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e.details))
@@ -43,8 +43,28 @@ def add_student(student_record: StudentRecord) -> dict:
 
 @student_router.put("/update")
 def update_student(student_record: StudentRecord):
-    ...
-    
+    try:
+        student_id: int = student_record.student_id
+        fName: str = student_record.fName
+        lName: str = student_record.lName
+        program: str = student_record.program
+        year: int = student_record.year
+
+        response = MonitorStudent.update_student(stud_id=student_id, new_fName=fName, 
+                                                new_lName=lName, new_program=program, new_year=year)
+
+        if response.data:
+            return {
+                "status_code": 200,
+                "message": f"Student {student_id} Updated Successfully"
+            }
+        
+    except APIError as e:
+        raise HTTPException(status_code=400, detail=str(e.details))
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e.details))
+
 
 @student_router.delete("/delete/{student_id}")
 def delete_student(student_id: int):
