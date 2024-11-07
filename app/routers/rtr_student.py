942d9ca8d241c55c499acdabc8f73f2df4deacfa
auxghlann <orenoytemail@gmail.com>
2024-11-07 20:56:38 +0800

    feat: Add a new add_student router

--- app/routers/rtr_student.py
+++ app/routers/rtr_student.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from app.core.monitorStudent import MonitorStudent
 from pydantic import BaseModel
+from postgrest.exceptions import APIError
 
 student_router = APIRouter(prefix="/student")
 
@@ -15,7 +16,29 @@ class StudentRecord(BaseModel):
 
 @student_router.post("/add")
 def add_student(student_record: StudentRecord) -> dict:
-    ...
+    
+    try:
+        student_id: int = student_record.student_id
+        fName: str = student_record.fName
+        lName: str = student_record.lName
+        program: str = student_record.program
+        year: int = student_record.year
+
+        record = MonitorStudent.add_student(stud_id=student_id, fName=fName, lName=lName, program=program, year=year)
+
+        if record.data:
+            return {
+                "status_code": 200,
+                "message": "Student Added Successfully"
+            }
+        
+    except APIError as e:
+        if e.code == '23505':
+            raise HTTPException(status_code=409, detail=str(e.details))
+
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e.details))
 
 
 @student_router.put("/update")
