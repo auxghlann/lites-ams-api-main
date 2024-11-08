e3f82ea7945ced00d666aaa67a0d84c966f30e42
auxghlann <orenoytemail@gmail.com>
2024-11-08 12:29:26 +0800

    feat: Add new routers to get and add attendance

--- app/routers/rtr_attendance.py
+++ app/routers/rtr_attendance.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException
 from app.core.monitorAttendance import MonitorAttendance
 from datetime import datetime
 from pydantic import BaseModel
+from postgrest.exceptions import APIError
 
 attendance_router = APIRouter(prefix="/attendance")
 
@@ -11,15 +12,41 @@ class AttendanceRecord(BaseModel):
 
 @attendance_router.post("/add")
 def add_attendance(attendance_record: AttendanceRecord):
-    ...
+    try:
+        student_id: int = attendance_record.student_id
+        time_status: int = attendance_record.time_status
+
+        response = MonitorAttendance.add_attendance(
+                                    stud_id=student_id, time_status=time_status)
+
+        if response.data:
+            return {
+                "status_code": 200,
+                "message": f"Attendance of Student {student_id} Added Successfully"
+            }
+        
+    except APIError as e:
+        raise HTTPException(status_code=400, detail=str(e.details))
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @attendance_router.put("/update")
 def update_attendance(attendance_record: AttendanceRecord):
+    #TODO()
     ...
 @attendance_router.delete("/delete/{student_id}")
 def delete_attendance(student_id: int):
+    #TODO()
     ...
 
 @attendance_router.get("/get")
 def get_all_attendance():
-    ...
+    try:
+        records = MonitorAttendance.get_attendances()
+        if not records:
+            raise HTTPException(status_code=404, detail="No attendace found")
+    
+        return records.data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
