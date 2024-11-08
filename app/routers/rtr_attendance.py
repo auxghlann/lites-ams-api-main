968b73f61753dcb688052fb4ce9bbf38e2a474bc
auxghlann <orenoytemail@gmail.com>
2024-11-08 21:44:05 +0800

    feat & refactor: Add new router to clear attendance and Add type-hints to every routers

--- app/routers/rtr_attendance.py
+++ app/routers/rtr_attendance.py
@@ -11,7 +11,7 @@ class AttendanceRecord(BaseModel):
     time_status: int | None
 
 @attendance_router.post("/add")
-def add_attendance(attendance_record: AttendanceRecord):
+def add_attendance(attendance_record: AttendanceRecord) -> dict:
     try:
         student_id: int = attendance_record.student_id
         time_status: int = attendance_record.time_status
@@ -37,7 +37,7 @@ def add_attendance(attendance_record: AttendanceRecord):
 #     !TBA if required
 #     ...
 @attendance_router.delete("/delete/{student_id}")
-def delete_attendance(student_id: int):
+def delete_attendance(student_id: int) -> dict:
     try:
         response = MonitorAttendance.delete_attendance(stud_id=student_id)
 
@@ -57,8 +57,22 @@ def delete_attendance(student_id: int):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@attendance_router.delete("/clear")
+def clear_attendance():
+    try:
+        response = MonitorAttendance.clear_attendance()
+        if response.data:
+            return {
+                "status_code": 200,
+                "message": "All attendance records cleared successfully"
+            }
+    except APIError as e:
+        raise HTTPException(status_code=400, detail=str(e.details))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 @attendance_router.get("/get")
-def get_all_attendance():
+def get_all_attendance() -> list[dict]:
     try:
         records = MonitorAttendance.get_attendances()
         if not records:
