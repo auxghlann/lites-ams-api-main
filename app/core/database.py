3a13fb455ee2bab209e192a9ea8c3f29a7b02f97
auxghlann <orenoytemail@gmail.com>
2024-11-09 20:25:02 +0800

    refactor: Change the env key names

--- app/core/database.py
+++ app/core/database.py
@@ -5,7 +5,7 @@ from supabase import create_client, Client
 
 load_dotenv()
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.environ.get("NEXT_PUBLIC_SUPABASE_URL")
+key: str = os.environ.get("NEXT_PUBLIC_SUPABASE_ANON_KEY")
 supabase: Client = create_client(url, key)
 
