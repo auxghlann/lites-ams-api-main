98dcdbe310d29c85bd1c94fd870aeaa3b04b9f12
auxghlann <orenoytemail@gmail.com>
2024-11-09 20:58:58 +0800

    refactor: change var names

--- app/core/database.py
+++ app/core/database.py
@@ -5,7 +5,7 @@ from supabase import create_client, Client
 
 load_dotenv()
 
-url: str = os.environ.get("NEXT_PUBLIC_SUPABASE_URL")
-key: str = os.environ.get("NEXT_PUBLIC_SUPABASE_ANON_KEY")
-supabase: Client = create_client(url, key)
+SUPABASE_URL: str = os.environ.get("SUPABASE_URL")
+SUPABASE_ANON_KEY: str = os.environ.get("SUPABASE_ANON_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
