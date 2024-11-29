d228b4bcdd7557d629e9501ed4e66cdcc686610c
auxghlann <orenoytemail@gmail.com>
2024-11-29 08:23:41 +0800

    feat: Add cors middleware for localhost

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import rtr_attendance, rtr_student
 
 app = FastAPI()
 app.include_router(rtr_attendance.attendance_router)
 app.include_router(rtr_student.student_router)
+
+
+# Add CORS middleware for localhost
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost", "http://127.0.0.1"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/')
