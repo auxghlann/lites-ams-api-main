85e7c96ac00bc30d4f7dbbaf1eb857503773c700
auxghlann <orenoytemail@gmail.com>
2025-01-23 12:17:33 +0800

    add cors middleware localhost url for testing

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ app.include_router(rtr_student.student_router)
 # Add CORS middleware for localhost
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://127.0.0.1", "http://localhost:5173"],
+    allow_origins=["http://localhost", "http://127.0.0.1", "http://localhost:5173",
+                    "http://127.0.0.1:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
